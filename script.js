d2986c6d8c8b632763e5abcbc9362aeb7dd18dcb
NadyaDysan <npryanishnikova18@gmail.com>
2022-10-23 20:19:07 +0300

    Added window.application

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-const form = document.querySelector('.form');
-        const formFieldInputs = form.querySelectorAll('.field-control');
-        const startButton = form.querySelectorAll('.button_start');
-        const startError = form.querySelector('.start_error');
+const form = document.querySelector(".form");
+const formFieldInputs = document.querySelectorAll(".field-control");
+const startButton = document.querySelectorAll(".button_start");
+const startError = document.querySelector(".start_error");
+const game = document.querySelector(".game");
 
-        function getFieldValue() {
-            for (const formFieldInput of formFieldInputs) {
-                if (formFieldInput.checked) {
-                    difficulty = formFieldInput.value;
-                    localStorage.setItem('difficulty', difficulty);
-                    console.log(difficulty);
-                    location.href = 'playing_field.html';
-                } else if (!formFieldInputs[0].checked && !formFieldInputs[1].checked && !formFieldInputs[2].checked) {
-                    startError.classList.remove("hidden");
-                }
-            }
-        };
+function getFieldValue() {
+  for (const formFieldInput of formFieldInputs) {
+    if (formFieldInput.checked) {
+      difficulty = formFieldInput.value;
+      window.application = { currentDifficulty: difficulty };
+      console.log(application.currentDifficulty);
+      form.classList.add("hidden");
+      game.classList.remove("hidden");
+    } else if (
+      !formFieldInputs[0].checked &&
+      !formFieldInputs[1].checked &&
+      !formFieldInputs[2].checked
+    ) {
+      startError.classList.remove("hidden");
+    }
+  }
+}
 
-        form.addEventListener('submit', (event) => {
-            event.preventDefault();
-            getFieldValue();
-        });
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  getFieldValue();
+});
