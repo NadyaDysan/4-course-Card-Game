2c340bb0b845060bd2331ffeaa072b8d77d9accf
NadyaDysan <npryanishnikova18@gmail.com>
2022-10-24 18:17:57 +0300

    Added 2 and 3 screens

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ const form = document.querySelector(".form");
 const formFieldInputs = document.querySelectorAll(".field-control");
 const startButton = document.querySelectorAll(".button_start");
 const startError = document.querySelector(".start_error");
+
 const game = document.querySelector(".game");
+const cards = document.querySelectorAll(".cards");
 
 function getFieldValue() {
   for (const formFieldInput of formFieldInputs) {
     if (formFieldInput.checked) {
       difficulty = formFieldInput.value;
-      window.application = { currentDifficulty: difficulty };
-      console.log(application.currentDifficulty);
-      form.classList.add("hidden");
-      game.classList.remove("hidden");
+      localStorage.setItem("difficulty", difficulty);
+      console.log(difficulty);
     } else if (
       !formFieldInputs[0].checked &&
       !formFieldInputs[1].checked &&
@@ -25,4 +25,14 @@ function getFieldValue() {
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   getFieldValue();
+  form.classList.add("hidden");
+  game.classList.remove("hidden");
+
+  setTimeout(() => {
+    cards.forEach((card) => {
+        card.classList.add("cards_cover");
+        startTimer();
+    });
+}, 5000);
+  
 });
